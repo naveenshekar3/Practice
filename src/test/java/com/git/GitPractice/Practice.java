a34c9c74868dde71f2e482a7b9861ce482fb41ef
Naveen <naveen@Naveens-MacBook-Pro.local>
2020-12-12 22:29:40 +0530

    Adding project from local to remote

---
+++ src/test/java/com/git/GitPractice/Practice.java
@@ -0,0 +1,14 @@
+package com.git.GitPractice;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	
+	@Test
+	public void start()
+	{
+		System.out.println("Printing from the start function");
+	}
+	
+	
+}
